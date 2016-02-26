fb05c2fd00e40ef5ef7b03d263fa38708d4d671c
Szymon Cimochowski <scimochowski@sigma.ug.edu.pl>
2016-02-26 10:37:36 +0100

    zad2 done

--- lab01/zad02.c
+++ lab01/zad02.c
@@ -1,2 +1,10 @@
 // Napisać ,,czysty'' program, czyli bez efektów ubocznych,
 // działający tak samo jak powyższa pętla for.
+
+#include<stdio.h>
+
+int main() {
+  char z;
+  while ((z = getchar()) !=EOF)
+  printf("%c", z)
+}
