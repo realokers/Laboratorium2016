a6acff2c6ba4a50b74ebccade05f09675696a8cb
Szymon Cimochowski <scimochowski@sigma.ug.edu.pl>
2016-04-22 11:05:01 +0200

    Update glowny.c

--- lab06/glowny.c
+++ lab06/glowny.c
@@ -40,27 +40,53 @@
 */
 
 #include "funkcje.h"
-
-int main(int argnum, char* arg[]) {
+int main(int argnum, char* arg[])
+{
   drzewo drz;
-  char tab[26] =
-    { /* a= */ '0',  /* b= */ '0',  /* c= */ '0',  /* d= */ '0',
-      /* e= */ '0',  /* f= */ '0',  /* g= */ '0',  /* h= */ '0',
-      /* i= */ '0',  /* j= */ '0',  /* k= */ '0',  /* l= */ '0',
-      /* m= */ '0',  /* n= */ '0',  /* o= */ '0',  /* p= */ '0',
-      /* q= */ '0',  /* r= */ '0',  /* s= */ '0',  /* t= */ '0',
-      /* u= */ '0',  /* v= */ '0',  /* w= */ '0',  /* x= */ '0',
-      /* y= */ '0',  /* z= */ '0'
-    };
-  if (argnum==1)  poziom=0;
-  else  poziom=2;
-
+  int tautologia = 1, spelnialna = 0;
+  char tab[10] =
+  { /* a= */ '0',  /* b= */ '0',  /* c= */ '0',  /* d= */ '0',
+    /* e= */ '0',  /* f= */ '0',  /* g= */ '0',  /* h= */ '0',
+    /* i= */ '0',  /* j= */ '0' };
+  if(argnum==1) poziom=0;
+  else poziom=2;
   nowy_lex();
-  if (form0(&drz)) {
-    printf("\n  wartosc: %c\n\n", wylicz(drz, tab));
+  if(form0(&drz))
+  {
+    int i;
+    for(i=0;i<1024 && (tautologia==1 || spelnialna==0);i++)
+    {
+      if(i%2==1) tab[0]='1'; else tab[0]='0';
+      if((i>>1)%2==1) tab[1]='1'; else tab[1]='0';
+      if((i>>2)%2==1) tab[2]='1'; else tab[2]='0';
+      if((i>>3)%2==1) tab[3]='1'; else tab[3]='0';
+      if((i>>4)%2==1) tab[4]='1'; else tab[4]='0';
+      if((i>>5)%2==1) tab[5]='1'; else tab[5]='0';
+      if((i>>6)%2==1) tab[6]='1'; else tab[6]='0';
+      if((i>>7)%2==1) tab[7]='1'; else tab[7]='0';
+      if((i>>8)%2==1) tab[8]='1'; else tab[8]='0';
+      if((i>>9)%2==1) tab[9]='1'; else tab[9]='0';
+      if(wylicz(drz, tab)=='0')
+      {
+        if(tautologia==1)
+        {
+          tautologia=0;
+          printf("\nNie jest tautologia, bo dla wartosciowania: a:%c b:%c c:%c d:%c e:%c f:%c g:%c h:%c i:%c j:%c wartosc wyrazenia jest rowna 0\n",tab[0],tab[1],tab[2],tab[3],tab[4],tab[5],tab[6],tab[7],tab[8],tab[9]);
+        }
+      }
+      else
+      {
+        if(spelnialna==0)
+        {
+          spelnialna=1;
+          printf("\nJest spelnialna, bo dla wartosciowania: a:%c b:%c c:%c d:%c e:%c f:%c g:%c h:%c i:%c j:%c wartosc wyrazenia jest rowna 1\n",tab[0],tab[1],tab[2],tab[3],tab[4],tab[5],tab[6],tab[7],tab[8],tab[9]);
+        }
+      }
+    }
   }
   else blad("nie formula");
-  if (lex != EOF && lex != '\n')  blad("smieci na koncu");
-
+  printf("\nTautologia: %s\n", tautologia ? "TAK" : "NIE");
+  printf("Spelnialna: %s\n\n", spelnialna ? "TAK" : "NIE");
+  if(lex!=EOF && lex!='\n') blad("smieci na koncu");
   return 0;
 }
